42a038c8ba2f35f88b59d0899279d4231ea3d1db
ZclipseCode <bmichaelbusiness@gmail.com>
2023-04-20 04:29:02 -0500

    shooting

--- Cowboy's Chorus/Assets/Scripts/PlayerShoot.cs
+++ Cowboy's Chorus/Assets/Scripts/PlayerShoot.cs
@@ -1,29 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
+    [SerializeField] Transform leftBulletPoint;
+    [SerializeField] Transform rightBulletPoint;
+    [SerializeField] float bulletSpeed;
     [SerializeField] float gunCooldown;
-    PlayerController pc;
-
-    void Start()
-    {
-        pc = GetComponent<PlayerController>();
-    }
+    bool onLeft;
+    bool onRight;
+    bool shootingLeft;
+    bool shootingRight;
 
     void Update()
     {
-        if (pc.GetLeft())
-            Debug.Log("left");
+        if (onLeft && !shootingLeft)
+            StartCoroutine(ShootLeft());
 
-        if (pc.GetRight())
-            Debug.Log("right");
+        if (onRight && !shootingRight)
+            StartCoroutine(ShootRight());
     }
 
-    void ShootLeft()
+    IEnumerator ShootLeft()
     {
+        shootingLeft = true;
+        GameObject b = Instantiate(bullet, leftBulletPoint.position, Quaternion.identity);
+        b.GetComponent<Rigidbody2D>().velocity = Vector2.left * bulletSpeed;
+        yield return new WaitForSeconds(gunCooldown);
+        shootingLeft = false;
+    }
 
+    IEnumerator ShootRight()
+    {
+        shootingRight = true;
+        GameObject b = Instantiate(bullet, rightBulletPoint.position, Quaternion.identity);
+        b.GetComponent<Rigidbody2D>().velocity = -Vector2.left * bulletSpeed;
+        yield return new WaitForSeconds(gunCooldown);
+        shootingRight = false;
+    }
+
+    public void OnLeft(InputAction.CallbackContext context)
+    {
+        onLeft = context.action.triggered;
+    }
+
+    public void OnRight(InputAction.CallbackContext context)
+    {
+        onRight = context.action.triggered;
     }
 }
